a9e6b6dcec0a92d595507ab0e5ea2a1947a38be5
Sasa Gornjakovic <sg31191@gmail.com>
2019-07-07 21:51:14 +0200

    Initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,23 +1,28 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+import { IonicModule } from '@ionic/angular';
+import { ChatInfoComponent } from './chat-info/chat-info.component';
 import { HomePage } from './home.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
-  ],
-  declarations: [HomePage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: HomePage,
+                children: [
+                    {
+                        path: 'chat-info'
+                    }
+                ]
+            }
+        ])
+    ],
+    declarations: [HomePage, ChatInfoComponent]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss']
 })
 export class HomePage {
+    constructor(public router: Router) {}
 
-  constructor() {}
+    showChatInfo(): boolean {
+        return this.router.url.indexOf('chat-info') > -1 ? true : false;
+    }
 
+    openChatInfo(): void {
+        this.router.navigate([{ outlets: { primary: 'home/chat-info' } }]);
+    }
 }
